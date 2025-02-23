5f364a9708a677a919e13c68505998aabf5941d7
olgasan93 <boonjian93@gmail.com>
2025-02-23 12:43:06 -0500

    final code

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        word_number = len(words)
-        print(f"{word_number} words found in the document")
+    import sys
+    from stats import get_book_text
+    from stats import word_counter
+    from stats import alphabet_counter
+    from stats import alphabet_sorter
+    
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-        characters = {}
-        words = file_contents.lower()
-        for a in words:
-            if a not in characters :
-                characters[a] = 1
-            else:
-                characters[a] += 1
+    with open(sys.argv[1]) as f:
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}")
+        book = get_book_text(f)
+        print("----------- Word Count ----------")
+        word_counter(book)
+        print("--------- Character Count -------")
+        alphabet_dict = alphabet_sorter(alphabet_counter(book))
+        for a in alphabet_dict:
+            if a["character"].isalpha() == True:
+                print(f"{a['character']}: {a['count']}")
+        print(f"============= END ===============")
+main()
+
+
+    # with open("books/frankenstein.txt") as f:
+    #     file_contents = f.read()
+    #     words = file_contents.split()
+    #     word_number = len(words)
+    #     print(f"{word_number} words found in the document")
+
+    #     characters = {}
+    #     words = file_contents.lower()
+    #     for a in words:
+    #         if a not in characters :
+    #             characters[a] = 1
+    #         else:
+    #             characters[a] += 1
         
-        characters_list = [{"character":key, "count":value} for key,value in characters.items() if key.isalpha() == True]
-        def sort_on(dict):
-            return dict["count"]
-        characters_list.sort(reverse=True, key=sort_on)
-        for dict in characters_list:
-            print(f"The '{dict['character']}' character was found {dict['count']} times")
+    #     characters_list = [{"character":key, "count":value} for key,value in characters.items() if key.isalpha() == True]
+    #     def sort_on(dict):
+    #         return dict["count"]
+    #     characters_list.sort(reverse=True, key=sort_on)
+    #     for dict in characters_list:
+    #         print(f"The '{dict['character']}' character was found {dict['count']} times")
 
 
 
@@ -28,8 +53,4 @@ def main():
         #     if char.isalpha() == False:
         #         del characters[char]
         # for char in characters:
-        #     print(f"The {char} character was found {characters[char]}times")
-        
-    
-        
-main()
+        #     print(f"The {char} character was found {characters[char]}times")           
---
+++ stats.py
@@ -0,0 +1,37 @@
+def get_book_text(filepath):
+    file_contents = filepath.read()
+    return file_contents
+
+def word_counter(book_string):
+    words = book_string.split()
+    num_words = len(words)
+    print(f"Found {num_words} total words")
+
+def alphabet_counter(book_string):
+    alphabets = {}
+    book_alphabet = book_string.lower()
+    for char in book_alphabet:
+        if char not in alphabets:
+            alphabets[char] = 1
+        else:
+            alphabets[char] += 1
+    return alphabets
+
+def alphabet_sorter(dict):
+    #Convert dictionary into sorted list of dictionaries
+    alphabet_list = [{"character":key, "count":value} for key,value in dict.items()]
+    
+    def sort_on(dict):
+        return dict["count"]
+    
+    alphabet_list.sort(reverse=True, key=sort_on)
+    return alphabet_list
+    print(alphabet_list)
+
+
+
+
+
+
+
+#I have key and value in my dictionary
